e67a615da8e920eae446e5ef9fb34b5b03c972fc
Your Name <you@example.com>
2022-03-14 16:31:25 +0100

    cheapSol

--- src/Find.java
+++ src/Find.java
@@ -2,12 +2,14 @@ import java.util.List;
 
 
 public class Find {
-
+String paraula;
     public Find(String s) {
-
+        this.paraula = s;
     }
 
     public boolean match(String stringPat) {
+        if (this.paraula.contains(stringPat) && stringPat != "")
+            return true;
         return false;
     }
 }
